4647e1a03c94e1e6d55db8ff76525b679ac2c49c
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-09-25 14:05:06 +0530

    Update [01] Joins.sql

--- [02] SQL/[01] Joins.sql
+++ [02] SQL/[01] Joins.sql
@@ -1,5 +1,5 @@
 SELECT c.[customer_key], c.[gender], c.[name], c.[city], c.[state_code], c.[state], c.[zip_code], c.[country], c.[continent], c.[birthday], 
-	   FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) AS age,
+       FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) AS age,
 	CASE 
 		WHEN FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) <= 13 THEN 'child'
 		WHEN FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) BETWEEN 13 AND 18 THEN 'teen'
@@ -13,17 +13,17 @@ SELECT c.[customer_key], c.[gender], c.[name], c.[city], c.[state_code], c.[stat
 	   s.[quantity] * p.[unit_cost_usd] AS total_cost, (s.[quantity] * p.[unit_price_usd]) - (s.[quantity] * p.[unit_cost_usd]) AS profit, 
 	   p.[subcategory_key], p.[subcategory], p.[category_key], p.[category], r.[date] AS exchange_rate_date, r.[currency], r.[exchange]
 
-FROM [Global_Retailer].[dbo].[customers] c
+FROM [Global_ELectronics_Retailer].[dbo].[customers] c
 
-JOIN [Global_Retailer].[dbo].[sales] s
+JOIN [Global_ELectronics_Retailer].[dbo].[sales] s
 ON c.customer_key = s.customer_key
 
-JOIN [Global_Retailer].[dbo].[stores] st
+JOIN [Global_ELectronics_Retailer].[dbo].[stores] st
 ON s.store_key = st.store_key
 
-JOIN [Global_Retailer].[dbo].[products] p
+JOIN [Global_ELectronics_Retailer].[dbo].[products] p
 ON p.product_key = s.product_key
 
-JOIN [Global_Retailer].[dbo].[exchange_rates] r
+JOIN [Global_ELectronics_Retailer].[dbo].[exchange_rates] r
 ON r.currency = s.currency_code
 AND r.date = s.order_date
