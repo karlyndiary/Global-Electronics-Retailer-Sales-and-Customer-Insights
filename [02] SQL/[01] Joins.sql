51c7c0cb2f512630782268c8892bd1e250c9bd5e
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-09-25 18:26:44 +0530

    Update [01] Joins.sql

--- [02] SQL/[01] Joins.sql
+++ [02] SQL/[01] Joins.sql
@@ -1,5 +1,5 @@
 SELECT c.[customer_key], c.[gender], c.[name], c.[city], c.[state_code], c.[state], c.[zip_code], c.[country], c.[continent], c.[birthday], 
-       FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) AS age,
+	   FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) AS age,
 	CASE 
 		WHEN FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) <= 13 THEN 'child'
 		WHEN FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) BETWEEN 13 AND 18 THEN 'teen'
@@ -7,7 +7,7 @@ SELECT c.[customer_key], c.[gender], c.[name], c.[city], c.[state_code], c.[stat
 		WHEN FLOOR(DATEDIFF(DAY, c.[birthday], GETDATE()) / 365.25) BETWEEN 36 AND 50 THEN 'adult'
 		ELSE 'senior'
 	END AS age_range,
-	   s.[order_number], s.[line_item], s.[order_date], s.[customer_key], s.[store_key], s.[product_key], s.[quantity], s.[currency_code],
+	   s.[order_number], s.[line_item], s.[order_date], s.[quantity],
 	   st.[store_key], st.[country] AS store_country, st.[state] AS store_state, st.[square_meters], st.[open_date],
 	   p.[product_key], p.[product_name], p.[brand], p.[color], p.[unit_cost_usd], p.[unit_price_usd], s.[quantity] * p.[unit_price_usd] AS revenue, 
 	   s.[quantity] * p.[unit_cost_usd] AS total_cost, (s.[quantity] * p.[unit_price_usd]) - (s.[quantity] * p.[unit_cost_usd]) AS profit, 
